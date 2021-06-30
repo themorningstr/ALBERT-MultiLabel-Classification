9f3753dd95700a1d1ccb16a4d70e280c99786d73
VISHWASH THAKUR <vishwasthakur35@gmail.com>
2021-06-30 18:33:26 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,18 +1,18 @@
 import transformers
 import torch
 
-MAX_LEN = 100
-EPOCH = 10
-MODEL_PATH = "../Save Model/"
-LOAD_MODEL = True
+MAX_LEN = 120
+EPOCH = 3
+MODEL_PATH = "../Save Model/model.bin"
 TRAINING_FILE = "../Data/train.csv"
-TESTING_FILE = "../Data/test.csv/"
+TESTING_FILE = "../Data/test.csv"
 TRAIN_BATCH_SIZE = 16
 VALID_BATCH_SIZE = 8
 TEST_BATCH_SIZE = 12
 NUMBER_OF_LABEL = 6
-MODEL_BASE_PATH = "albert-large-v2"
+MODEL_BASE_PATH = "bert-base-multilingual-uncased"
 CLASSIFIER_DROPOUT_PROB = 0.3
 HIDDEN_SIZE = 1024
-TOKENIZER = transformers.AlbertTokenizer.from_pretrained(MODEL_BASE_PATH,do_lower_case = True)
+TOKENIZER = transformers.BertTokenizer.from_pretrained(MODEL_BASE_PATH,do_lower_case = True)
 DEVICE = "cuda" if torch.cuda.is_available() else "cup "
+THRESHOLD = 0.5
