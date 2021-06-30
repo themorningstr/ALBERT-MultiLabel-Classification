00cbfc445d8eed7beb6a0f5e422b0949c044a4ac
VISHWASH THAKUR <vishwasthakur35@gmail.com>
2021-06-30 18:35:38 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,22 +1,19 @@
 import torch
-import transformers
 import Config
+import transformers
+import torch.nn as nn
 
 
-class TOXICMODEL(transformers.AlbertPreTrainedModel):
-    def __init__(self):
+class TOXICMODEL(nn.Module):
+    def __init__(self,conf):
         super(TOXICMODEL,self).__init__()
-        self.Num_Label = Config.NUMBER_OF_LABEL
-        self.config = transformers.AlbertConfig.from_pretrained(Config.MODEL_BASE_PATH)
-        self.albert = transformers.AlbertForSequenceClassification.from_pretrained(self.config,return_dict = False)
+        self.conf = conf
+        self.bert = transformers.BertModel.from_pretrained(self.conf)
         self.dropout = torch.nn.Dropout(Config.CLASSIFIER_DROPOUT_PROB)
-        self.classfier = torch.nn.Linear(Config.HIDDEN_SIZE,self.Num_Label)
-        self.init_weights()
+        self.classifier = torch.nn.Linear(Config.HIDDEN_SIZE,Config.NUMBER_OF_LABEL)
 
-    def forward(self,input_ids,attention_mask,token_type_ids,labels):
-        output = self.albert(input_ids,attention_mask,token_type_ids,labels)
-        pooled_output = output[1]
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)   
-        return logits 
-         
+    def forward(self,input_ids,attention_mask,token_type_ids):
+        _, output = self.bert(input_ids,attention_mask,token_type_ids,return_dict = False)
+        output = self.dropout(output)
+        output = self.classifier(output)   
+        return output 
